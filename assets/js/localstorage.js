e0a72e11d150f30332e52dd8ba4136373ccc7843
TheDreamTeam5 <kaykyuu@gmail.com>
2015-11-23 11:44:42 -0800

    Update localstorage.js

--- assets/js/localstorage.js
+++ assets/js/localstorage.js
@@ -1,1 +1,11 @@
+function retrieveQuestions(p1, p2) {
+    return p1 * p2;              // The function returns the product of p1 and p2
+}
 
+function addQuestion(p1, p2) {
+    return p1 * p2;              // The function returns the product of p1 and p2
+}
+
+function incrementQuestion(p1, p2) {
+    return p1 * p2;              // The function returns the product of p1 and p2
+}
